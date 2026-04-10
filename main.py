517ee5a3a33ecef76e1fe76cc32346e31cbd2060
Andreas Heine <andreas_heine@gmx.net>
2026-04-10 08:14:39 +0200

    fix: Browsename to DefaultInstanceBrowseName

--- main.py
+++ main.py
@@ -145,7 +145,8 @@ async def main():  # pylint: disable=too-many-statements
         print(f"Found OPC for Machinery JobManager node: {await job_manager_node.read_display_name()}")
     except ua.UaError as ex:
         print(f"Error finding JobManager node in Machinery BuildingBlocks: {ex}")
-        job_manager_node: Node = await building_blocks_node.get_child(f"{machinery_jobs_index}:JobManager")
+        # OPC for Machinery: DefaultInstanceBrowseName
+        job_manager_node: Node = await building_blocks_node.get_child(f"{machinery_jobs_index}:JobManagement")
         print(f"Found OPC for Machinery JobManager node: {await job_manager_node.read_display_name()}")
 
     job_order_results_node: Node = await job_manager_node.get_child(f"{machinery_jobs_index}:JobOrderResults")
